e74823fdb6db8d545892e7c7f96d6da002f20e1f
ai2playgame <detective9684@gmail.com>
2022-09-09 02:06:01 +0900

    Gitコマンド実行に必要なインターフェースを整えた

--- Assets/UGitInfoHolder/Editor/GitCommandExecutor.cs
+++ Assets/UGitInfoHolder/Editor/GitCommandExecutor.cs
@@ -1,7 +1,25 @@
-﻿namespace Editor
+﻿namespace UGitInfoHolder.Editor
 {
-    public class GitCommandExecutor
+    public static class GitCommandExecutor
     {
-        
+        public static bool CanExecute() => false;
+        public static string ExecuteGitCommand(string arguments) => string.Empty;
+
+        private class ExecutionResult
+        {
+            public string Stdout;
+            public string Stderr;
+            public int ExitCode;
+
+            public ExecutionResult(string stdout, string stderr, int exitCode)
+            {
+                Stdout = stdout;
+                Stderr = stderr;
+                ExitCode = exitCode;
+            }
+            public bool IsSuccess => ExitCode != 0;
+        }
+
+        private static ExecutionResult ExecuteCommand(string arguments) => null;
     }
 }
