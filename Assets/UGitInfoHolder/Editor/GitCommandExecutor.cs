672744304abfa5ae7359f62e5278b074a1946017
ai2playgame <detective9684@gmail.com>
2022-09-11 23:11:35 +0900

    wip

--- Assets/UGitInfoHolder/Editor/GitCommandExecutor.cs
+++ Assets/UGitInfoHolder/Editor/GitCommandExecutor.cs
@@ -1,4 +1,8 @@
-﻿namespace UGitInfoHolder.Editor
+﻿using System;
+using UnityEngine;
+using Application = UnityEngine.Device.Application;
+
+namespace UGitInfoHolder.Editor
 {
     public static class GitCommandExecutor
     {
@@ -11,8 +15,8 @@
         
         public class ExecutionResult
         {
-            public string Stdout;
-            public string Stderr;
+            public readonly string Stdout;
+            public readonly string Stderr;
             public readonly int ExitCode;
 
             public ExecutionResult(string stdout, string stderr, int exitCode)
@@ -24,7 +28,69 @@
             public bool IsNotError => ExitCode != 0;
         }
 
-        private static ExecutionResult ExecuteGitCommand(string arguments, float timeoutSec = 10.0f) => null;
-        private static ExecutionResult ExecuteCommand(string arguments, float timeoutSec = 10.0f) => null;
+        public static ExecutionResult ExecuteGitCommand(string arguments, float timeoutSec = 10.0f)
+        {
+            if (!CanExecute())
+            {
+                Debug.LogError("This computer can't execute git command.");
+                return null;
+            }
+
+            return ExecuteCommand($"git {arguments}", timeoutSec);
+        }
+
+        private static ExecutionResult ExecuteCommand(string arguments, float timeoutSec = 10.0f)
+        {
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsEditor:
+                    return ExecuteCommandInWindows(arguments, timeoutSec);
+                
+                default:
+                    throw new NotSupportedException($"this platform ({Application.platform}) is not supported.");
+            }
+        }
+
+        private static ExecutionResult ExecuteCommandInWindows(string arguments, float timeoutSec = 10.0f)
+        {
+            using (var process = new System.Diagnostics.Process())
+            {
+                var cmdPath = System.Environment.GetEnvironmentVariable("ComSpec");
+                if (string.IsNullOrEmpty(cmdPath))
+                {
+                    Debug.LogError("Couldn't find cmd.exe");
+                    return null;
+                }
+                process.StartInfo.FileName = cmdPath;
+                process.StartInfo.Arguments = $"/c{arguments}";
+                
+                // Prevent to show window
+                process.StartInfo.CreateNoWindow = true;
+                
+                // Enable to read command's output
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                // process.StartInfo.RedirectStandardError = true; // TODO: 標準出力と標準エラーを同時に取得する方法を探る
+                process.StartInfo.RedirectStandardInput = false;
+
+                process.Start();
+                
+                // Read stdout and stderr
+                string stdout = process.StandardOutput.ReadToEnd();
+
+                if (!process.WaitForExit((int)(timeoutSec * 1000)))
+                {
+                    // タイムアウトにつきプロセスを強制終了する
+                    Debug.LogError("timeout");
+                    process.Kill();
+                    process.WaitForExit();
+                    return null;
+                }
+                
+                process.Close();
+
+                return new ExecutionResult(stdout, string.Empty, process.ExitCode);
+            }
+        }
     }
 }
