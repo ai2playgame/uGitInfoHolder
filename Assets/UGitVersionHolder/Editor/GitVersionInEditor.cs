ca70420a8b18a57403e670333616953b3ea2b162
ai2playgame <detective9684@gmail.com>
2022-10-07 00:00:36 +0900

    refactor

--- Assets/UGitVersionHolder/Editor/GitVersionInEditor.cs
+++ Assets/UGitVersionHolder/Editor/GitVersionInEditor.cs
@@ -7,41 +7,49 @@ namespace UGitVersionHolder.Editor
 {
     public class GitVersionInEditor
     {
-        public string Hash;
-        public string Date;
+        private string _hash;
+        private string _date;
 
         public static GitVersionInEditor Generate()
+        {
+            return new GitVersionInEditor()
+            {
+                _hash = GetHash(),
+                _date = GetDate(),
+            };
+        }
+
+        private static string GetHash()
         {
             var revParseResult = GitCommandExecutor.ExecuteGitCommand("rev-parse --short HEAD");
             var hash = revParseResult.Stdout;
             hash = hash.Replace("\n", "").Replace("\r", "");
+            return hash;
+        }
 
+        private static string GetDate()
+        {
             var dateResult =
                 GitCommandExecutor.ExecuteGitCommand(
                     "log --date=iso --date=format:\"%m%d\" --pretty=format:\"%ad\" -1");
             var date = dateResult.Stdout;
             date = date.Replace("\n", "").Replace("\r", "");
-            
-            return new GitVersionInEditor()
-            {
-                Hash = hash,
-                Date = date,
-            };
+            return date;
         }
 
         public GitVersionContent ConvertToRuntimeScriptableObject()
         {
             var versionAsset = ScriptableObject.CreateInstance<GitVersionContent>();
-            versionAsset.hash = Hash;
-            versionAsset.date = Date;
+            versionAsset.hash = _hash;
+            versionAsset.date = _date;
 
             return versionAsset;
         }
 
         public static GitVersionInEditor Invalid => new GitVersionInEditor()
         {
-            Hash = "INVALID",
-            Date = "9999",
+            _hash = "INVALID",
+            _date = "9999",
         };
     }
 }
